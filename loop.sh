770f95040ec8d07e548051e1c77ad8325a950f50
Matrixsun <ziwusun@gmail.com>
2016-10-18 11:37:24 +0800

    Add files via upload

---
+++ loop.sh
@@ -0,0 +1,22 @@
+#--------------------------
+#	function:test fio every day
+#	mails:sunziwu@didichuxing.com
+#	date:2016-09-16
+#
+#
+#
+#----------------------------------
+#!/bin/bash
+#ps -fe|grep loop.sh|grep -v grep|awk '{print $2}'|xargs kill -9
+killall iostat
+killall fio
+Date=`date +%Y%m%d`
+nohup iostat -xm 2  >>/data0/sunziwu/diskTest/data/abrasion/$Date.log &
+/usr/bin/fio /data0/sunziwu/diskTest/script/job_100_rw4k
+killall iostat 
+
+sleep 10
+
+/bin/sh /data0/sunziwu/diskTest/script/sample.sh
+
+killall iostat
---
+++ sample.sh
@@ -0,0 +1,30 @@
+#-----------------------------
+#	function: 标准测试fio
+#	mails:sunziwu@didichuxing.com
+#	date:2016-09-15
+#
+#
+#
+#-----------------------------------------
+#!/bin/bash
+killall fio
+killall iostat
+Date=`date +%Y%m%d`
+mkdir -p /data0/sunziwu/diskTest/data/sample/$Date
+
+for bsSize in {1k,4k,7k,64k,1024k}
+do
+	for rwModle in {read,randread,write,randwrite}
+	do
+		nohup iostat -xm 2 >>/data0/sunziwu/diskTest/data/sample/$Date/$bsSize-$rwModle.log &
+		for sd in {sdb,sdc,sdh,sdi,sdj,sdk}
+		do
+	
+		nohup /usr/bin/fio --name=global --ioengine=libaio --bs=$bsSize --rw=$rwModle  --filename=/dev/$sd   --runtime=600 --direct=1 -numjobs=4  -iodepth=32 --name=job --group_reporting	&
+		
+		done
+		sleep 603
+		killall iostat
+	done		
+done
+
